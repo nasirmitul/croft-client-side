33612f6c7fe9503e892f277535144117128ca383
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-08 23:46:48 +0600

    collected and sent new service data to the backend

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const AddService = () => {
 
+    const time = new Date().toLocaleTimeString();
+    const [ctime, setDate] = useState(time);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const form = e.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        const image = form.image.value;
 
-        console.log(name, email, password, image);
+        let time = new Date().toLocaleTimeString();
+        setDate(time);
+
+        const form = e.target;
+
+        const service_name = form.service_name.value;
+        const service_img = form.service_img.value;
+        const service_price = form.service_price.value;
+        const service_ratings = form.service_ratings.value;
+        const service_description = form.service_description.value;
+
+        //have to get the current time
+        console.log(service_name, service_img, service_price, service_ratings, service_description, ctime);
+
+        const addNewService = {
+            serviceName: service_name,
+            serviceImage: service_img,
+            servicePrice: service_price,
+            serviceRatings: service_ratings,
+            serviceDescription: service_description
+        }
+
+        console.log(addNewService);
+
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(addNewService)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.acknowledged){
+                    form.reset();
+                    alert('service added successfully');
+                }
+            })
+            .catch(err => console.log(err))
     }
 
     return (
@@ -28,8 +65,7 @@ const AddService = () => {
                     <input type="number" placeholder='Ratings' name='service_ratings' required />
                     <textarea name="service_description" placeholder='Service Description' required></textarea>
                 </div>
-
-                <button className='custom-button' type='submit'>Signup</button>
+                <button className='custom-button' type='submit'>Add new Service</button>
             </form>
         </div>
     );
