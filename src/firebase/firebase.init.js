07f6b672437ca8ec78b902c14980ac2b0f366f89
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-10 16:50:53 +0600

    environment variable complete

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAp3mOpJvLjLE3jnFhpvhrmvmdEcKUWcBE",
-    authDomain: "croft1.firebaseapp.com",
-    projectId: "croft1",
-    storageBucket: "croft1.appspot.com",
-    messagingSenderId: "645702374899",
-    appId: "1:645702374899:web:66d328fbb80a8ac8c5ee86"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
